d5583dbe9b4cde75501845339a3246211c7f840e
Leandro Rocha <leandroamrocha@gmail.com>
2024-09-12 09:31:21 -0300

    primeiro modulo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario/index.js';
 
 function App() {
   return (
     <div>
       <Banner />
+      <Formulario />
     </div>
   );
 }
