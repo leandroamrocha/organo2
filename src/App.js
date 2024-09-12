72b7d09e215cfb99f5bd5d86218458119ae86f90
Leandro Rocha <leandroamrocha@gmail.com>
2024-09-12 18:51:02 -0300

    aula1pt4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import Banner from './componentes/Banner/Banner.js';
 import Formulario from './componentes/Formulario/index.js';
 import { useState } from 'react';
+import Team from './componentes/Team';
 
 function App() {
 
   const [colaboradores, setColaboradores] = useState([]);
 
-  const aoNovoColaboradorCadastrado = (colaborador) => {
-    console.log(colaborador);
-    setColaboradores([...colaboradores, colaborador]) 
-   }
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
 
   return (
-    <div>
+    <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorCadastrado(colaborador)} />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Team nome="Programação" />
+      <Team nome="Front-End" />
+      <Team nome="Data Science" />
     </div>
-  );
+  )
 }
 
 export default App;
